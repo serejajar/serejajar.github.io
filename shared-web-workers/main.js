2a1aaf9714a7b4f9e2d21556cbb30619d65c8604
Sergey Jar <serejajar@yandex.ru>
2019-10-20 13:12:16 +0300

    shared WW - added onerror event

--- shared-web-workers/main.js
+++ shared-web-workers/main.js
@@ -4,9 +4,16 @@ const p = document.querySelector('p.error-msg');
 if (window.SharedWorker) {
   const myWorker = new SharedWorker('worker.js');
 
-  myWorker.port.postMessage(['value0', 'value1']);
+  document.body.onclick = function() {
+    myWorker.port.postMessage(['value0', 'value1']);
+    console.log('Message posted to worker.js');
+  }
+
   myWorker.port.onmessage = function(e) {
     console.log('Message received from worker', e);
+  }
+  myWorker.port.onerror = function(e) {
+    console.log('Error received from worker', e);
   }
 } else {
   wrapper.style.display = 'none';
