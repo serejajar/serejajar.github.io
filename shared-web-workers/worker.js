28e93890b2eca7bf84aa50b901daf0e40f079e2c
Sergey Jar <serejajar@yandex.ru>
2019-10-20 13:14:27 +0300

    shared WW - removed setInterval

--- shared-web-workers/worker.js
+++ shared-web-workers/worker.js
@@ -4,12 +4,4 @@ onconnect = function(e) {
     console.log('port.onmessage', e);
     port.postMessage('workerResult');
   }
-
-  port.postMessage('Start setInterval');
-
-  let i = 0;
-  setInterval(function() {
-    console.log('Interval ' + i);
-    port.postMessage('workerResult ' + ++i);
-  }, 2000)
 }
