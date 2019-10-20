294829c98d2466fc946981880c86c864721fb908
Sergey Jar <serejajar@yandex.ru>
2019-10-20 12:50:42 +0300

    shared WW - removed unused code

--- shared-web-workers/main.js
+++ shared-web-workers/main.js
@@ -1,13 +1,7 @@
-// const btn = document.querySelector('button');
-
 if (window.SharedWorker) {
   const myWorker = new SharedWorker('worker.js');
 
-  // btn.onclick = function() {
-  //   myWorker.port.postMessage(['value0', 'value1']);
-  //   console.log('Message posted to worker');
-  // }
-
+  myWorker.port.postMessage(['value0', 'value1']);
   myWorker.port.onmessage = function(e) {
     console.log('Message received from worker', e.data[0]);
   }
--- shared-web-workers/worker.js
+++ shared-web-workers/worker.js
@@ -1,10 +1,11 @@
 onconnect = function(e) {
-  console.log('Onconnect works!', e);
   const [ port ] = e.ports[0];
   port.onmessage = function(e) {
     port.postMessage('workerResult');
   }
 
+  port.postMessage('Start setInterval');
+  
   let i = 0;
   setInterval(function() {
     port.postMessage('workerResult' + ++i);
