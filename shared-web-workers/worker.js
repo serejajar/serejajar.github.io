3e41c711761618cbc1f33d4615e3fe533b825b6b
Sergey Jar <serejajar@yandex.ru>
2019-10-20 13:03:47 +0300

    shared WW - added logs

--- shared-web-workers/worker.js
+++ shared-web-workers/worker.js
@@ -1,13 +1,15 @@
 onconnect = function(e) {
   const [ port ] = e.ports[0];
   port.onmessage = function(e) {
+    console.log('port.onmessage', e);
     port.postMessage('workerResult');
   }
 
   port.postMessage('Start setInterval');
-  
+
   let i = 0;
   setInterval(function() {
-    port.postMessage('workerResult' + ++i);
+    console.log('Interval ' + i);
+    port.postMessage('workerResult ' + ++i);
   }, 2000)
 }
