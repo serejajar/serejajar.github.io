c2f87f9ae75151269c5ea751b6aac2c286481b3f
Sergey Jar <serejajar@yandex.ru>
2019-10-20 13:44:27 +0300

    shared WW - added for loop with logs

--- shared-web-workers/worker.js
+++ shared-web-workers/worker.js
@@ -1,12 +1,11 @@
 onconnect = function(e) {
   const port = e.ports[0];
-  console.log('e.ports', e.ports[0]);
 
   port.onmessage = function(e) {
-    console.log('e', e);
-    const workerResult = 'Result';
-    port.postMessage(workerResult);
+    port.postMessage(e.data[0]);
   }
 
-  port.postMessage('e.data');
+  for (var i = 0; i < 100000000; i++) {
+    port.postMessage('e.data ' + i);
+  }
 }
