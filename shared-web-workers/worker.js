b3f6d04bec86b44122ffe51e1a0ed83dbb00a766
Sergey Jar <serejajar@yandex.ru>
2019-10-20 14:11:53 +0300

    Shared web worker - tiny fix

--- shared-web-workers/worker.js
+++ shared-web-workers/worker.js
@@ -1,12 +1,14 @@
 onconnect = function(e) {
   const port = e.ports[0];
 
+  console.log('e.ports', e.ports);
+
   port.onmessage = function(e) {
     console.log('onmessage', e);
     port.postMessage(e.data[0]);
   }
 
   setInterval(function() {
-    port.postMessage('e.data ' + i);
+    port.postMessage('e.data');
   }, 3000);
 }
