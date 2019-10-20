260196d5efda45c74ef8efeeb4aa8456c4ff5209
Sergey Jar <serejajar@yandex.ru>
2019-10-20 20:56:03 +0300

    Shared WW - tiny fix

--- shared-web-workers/other.js
+++ shared-web-workers/other.js
@@ -16,7 +16,6 @@ if (window.SharedWorker) {
     console.log('Error from worker.js', e)
   }
 
-  // myWorker.port.postMessage('other.js');
 } else {
   wrapper.style.display = 'none';
   p.style.display = 'block';
