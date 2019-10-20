a8619c4438faf991b0d2bce3e3eb7417ea9716a6
Sergey Jar <serejajar@yandex.ru>
2019-10-20 14:08:02 +0300

    Shared web worker - fix interval

--- shared-web-workers/worker.js
+++ shared-web-workers/worker.js
@@ -2,10 +2,11 @@ onconnect = function(e) {
   const port = e.ports[0];
 
   port.onmessage = function(e) {
+    console.log('onmessage', e);
     port.postMessage(e.data[0]);
   }
 
-  setTimeout(function functionName() {
+  setInterval(function() {
     port.postMessage('e.data ' + i);
   }, 3000);
 }
