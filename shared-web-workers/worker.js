19038dd1b89beb7f9cfd7a17e33065c9c7543f87
Sergey Jar <serejajar@yandex.ru>
2019-10-20 14:28:13 +0300

    Shared web worker - added second script

--- shared-web-workers/worker.js
+++ shared-web-workers/worker.js
@@ -1,13 +1,10 @@
 onconnect = function(e) {
   const port = e.ports[0];
 
-  console.log('e.ports', e.ports);
-
   port.onmessage = function(e) {
-    console.log('onmessage', e);
     port.postMessage(e.data[0]);
   }
-  
+
   let i = 0;
   setInterval(function() {
     port.postMessage('e.data ' + ++i);
