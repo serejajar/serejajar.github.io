3aa899876b4e8d4783b809ccf3e897f100142254
Sergey Jar <serejajar@yandex.ru>
2019-10-20 12:57:51 +0300

    shared WW - removed other.html

--- shared-web-workers/main.js
+++ shared-web-workers/main.js
@@ -1,10 +1,14 @@
+const wrapper = document.querySelector('div');
+const p = document.querySelector('p.error-msg');
+
 if (window.SharedWorker) {
   const myWorker = new SharedWorker('worker.js');
 
   myWorker.port.postMessage(['value0', 'value1']);
   myWorker.port.onmessage = function(e) {
-    console.log('Message received from worker', e.data[0]);
+    console.log('Message received from worker', e);
   }
 } else {
-  console.log('WW not works');
+  wrapper.style.display = 'none';
+  p.style.display = 'block';
 }
--- shared-web-workers/other.js
+++
@@ -1,9 +0,0 @@
-if (window.SharedWorker) {
-  const myWorker = new SharedWorker('worker.js');
-
-  myWorker.port.onmessage = function(e) {
-    console.log('Message received from worker', e.data[0]);
-  }
-} else {
-  console.log('WW not works');
-}
