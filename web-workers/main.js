cbc0201cd1cc472461f401ffc2a89b083ac61d0b
Sergey Jar <serejajar@yandex.ru>
2019-10-20 12:19:49 +0300

    web workers - tiny fix

--- web-workers/main.js
+++ web-workers/main.js
@@ -1,7 +1,8 @@
+const wrapper = document.querySelector('div');
 const input = document.querySelector('input');
 const btn = document.querySelector('button');
 const result = document.getElementById('result')
-const p = document.querySelector('p');
+const p = document.querySelector('p.error-msg');
 
 if (window.Worker) {
   const myWorker = new Worker('worker.js');
@@ -16,6 +17,6 @@ if (window.Worker) {
     result.innerText = e.data;
   }
 } else {
-  input.style.display = btn.style.display = 'none';
+  wrapper.style.display = 'none';
   p.style.display = 'block';
 }
