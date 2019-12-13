cca98371105fd6a91959b1aced14e8ab4838a7f7
Sergey Jar <serejajar@yandex.ru>
2019-12-13 12:24:57 +0200

    Service-Workers - added test response

--- service-workers/sw.js
+++ service-workers/sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('v1').then((cache) => {
+    caches.open('name').then((cache) => {
       return cache.addAll([
         './index.html',
         './app.js',
@@ -11,23 +11,26 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('activate', (event) => {
-  console.log('activate', event);
+  console.log('activate', event.request);
 });
 
 self.addEventListener('message', (event) => {
-  console.log('message', event);
+  console.log('message', event.request);
 });
 
 
 
 self.addEventListener('fetch', (event) => {
-  console.log('fetch', event);
+  console.log('fetch', event.request);
+  new Response('<p>Hello from your friendly neighbourhood service worker!</p>', {
+    headers: { 'Content-Type': 'text/html' }
+  });
 });
 
 self.addEventListener('sync', (event) => {
-  console.log('sync', event);
+  console.log('sync', event.request);
 });
 
 self.addEventListener('push', (event) => {
-  console.log('push', event);
+  console.log('push', event.request);
 });
