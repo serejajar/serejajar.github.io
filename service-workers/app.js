5c1a9620b2b267ca7ce668b011fc4319190ace93
Sergey Jar <serejajar@yandex.ru>
2019-12-13 18:17:54 +0200

    Service-Workers - fixed cache

--- service-workers/app.js
+++ service-workers/app.js
@@ -9,54 +9,56 @@ if ('serviceWorker' in navigator) {
 
 const inputs = document.querySelectorAll('input[type="checkbox"]');
 
-cache.keys().then(function(keys) {
-  const urls = keys.map(function(item) {
-    return item.url;
-  });
-
-  for (var i = 0; i < inputs.length; i++) {
-    const parent = inputs[i].parentNode.parentNode;
-    const checkbox = inputs[i];
-    const messageEl = parent.querySelector('span');
-    const dataImg = parent.getAttribute('data-image');
-    const name = `./gallery/${dataImg}.jpg`;
-
-    urls.find(url => {
-      console.log(url.indexOf(name));
+caches.open('test-SW').then(function(cache) {
+  cache.keys().then(function(keys) {
+    const urls = keys.map(function(item) {
+      return item.url;
     });
 
-    if (urls.find(url => url.indexOf(name) !== -1)) {
-      checkbox.checked = true;
-      messageEl.innerHTML = 'Saved';
-    } else {
-      checkbox.checked = false;
-      messageEl.innerHTML = 'Not saved';
-    }
+    for (var i = 0; i < inputs.length; i++) {
+      const parent = inputs[i].parentNode.parentNode;
+      const checkbox = inputs[i];
+      const messageEl = parent.querySelector('span');
+      const dataImg = parent.getAttribute('data-image');
+      const name = `./gallery/${dataImg}.jpg`;
 
-    checkbox.onclick = function() {
-      if (checkbox.checked) {
-        caches.open('test-SW').then(function(cache) {
-          cache.add(name).then(function() {
-            messageEl.innerHTML = 'Saved';
+      urls.find(url => {
+        console.log(url.indexOf(name));
+      });
 
-            cache.keys().then(function(keys) {
-              console.log('keys', keys);
-            });
-          });;
-        })
+      if (urls.find(url => url.indexOf(name) !== -1)) {
+        checkbox.checked = true;
+        messageEl.innerHTML = 'Saved';
       } else {
-        caches.open('test-SW').then(function(cache) {
-          cache.delete(name).then(function(isDeleted) {
-            if (isDeleted) {
-              messageEl.innerHTML = 'Not saved';
-            }
+        checkbox.checked = false;
+        messageEl.innerHTML = 'Not saved';
+      }
 
-            cache.keys().then(function(keys) {
-              console.log('keys', keys);
+      checkbox.onclick = function() {
+        if (checkbox.checked) {
+          // caches.open('test-SW').then(function(cache) {
+            cache.add(name).then(function() {
+              messageEl.innerHTML = 'Saved';
+
+              cache.keys().then(function(keys) {
+                console.log('keys', keys);
+              });
+            });;
+          // });
+        } else {
+          // caches.open('test-SW').then(function(cache) {
+            cache.delete(name).then(function(isDeleted) {
+              if (isDeleted) {
+                messageEl.innerHTML = 'Not saved';
+              }
+
+              cache.keys().then(function(keys) {
+                console.log('keys', keys);
+              });
             });
-          });
-        })
+          // });
+        }
       }
     }
-  }
+  });
 });
