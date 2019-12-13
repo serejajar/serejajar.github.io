b3c29247f747e4be7d045bdd2e61d16c12417af4
Sergey Jar <serejajar@yandex.ru>
2019-12-13 12:48:41 +0200

    Service-Workers - added code for check SW

--- service-workers/sw.js
+++ service-workers/sw.js
@@ -1,10 +1,11 @@
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('name').then((cache) => {
+    caches.open('v1').then((cache) => {
       return cache.addAll([
         './index.html',
         './app.js',
         './gallery/tower-clock.jpg',
+        './gallery/default.jpg',
       ]);
     })
   );
@@ -20,11 +21,24 @@ self.addEventListener('message', (event) => {
 
 
 
-self.addEventListener('fetch', (event) => {
-  console.log('fetch', event.request);
-  new Response('<p>Hello from your friendly neighbourhood service worker!</p>', {
-    headers: { 'Content-Type': 'text/html' }
-  });
+self.addEventListener('fetch', function(event) {
+  event.respondWith(caches.match(event.request).then(function(response) {
+    return new Response('Hello from your friendly neighbourhood service worker!');
+    // if (response !== undefined) {
+    //   return response;
+    // } else {
+    //   return fetch(event.request).then(function (response) {
+    //     let responseClone = response.clone();
+    //
+    //     caches.open('v1').then(function (cache) {
+    //       cache.put(event.request, responseClone);
+    //     });
+    //     return response;
+    //   }).catch(function () {
+    //     return caches.match('/gallery/default.jpg');
+    //   });
+    // }
+  }));
 });
 
 self.addEventListener('sync', (event) => {
