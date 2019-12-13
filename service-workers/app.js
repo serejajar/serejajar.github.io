61c362618feac01316b11c7f5ff8d0d362d20777
Sergey Jar <serejajar@yandex.ru>
2019-12-13 18:13:25 +0200

    Service-Workers - added checking/unchecking checkboxes on page

--- service-workers/app.js
+++ service-workers/app.js
@@ -9,36 +9,48 @@ if ('serviceWorker' in navigator) {
 
 const inputs = document.querySelectorAll('input[type="checkbox"]');
 
-for (var i = 0; i < inputs.length; i++) {
-  inputs[i].onclick = function() {
-    const checkbox = this;
-    const parent = checkbox.parentNode.parentNode;
+cache.keys().then(function(keys) {
+  const urls = keys.map(function(item) {
+    return item.url;
+  });
+
+  for (var i = 0; i < inputs.length; i++) {
+    const parent = inputs[i].parentNode.parentNode;
+    const checkbox = inputs[i];
     const messageEl = parent.querySelector('span');
     const dataImg = parent.getAttribute('data-image');
     const name = `./gallery/${dataImg}.jpg`;
 
-    if (checkbox.checked) {
-      caches.open('test-SW').then(function(cache) {
-        cache.add(name).then(function() {
-          messageEl.innerHTML = 'Saved';
+    urls.find(url => {
+      console.log(url.indexOf(name));
+    });
 
-          cache.keys().then(function(keys) {
-            console.log('keys', keys);
-          });
-        });;
-      })
-    } else {
-      caches.open('test-SW').then(function(cache) {
-        cache.delete(name).then(function(isDeleted) {
-          if (isDeleted) {
-            messageEl.innerHTML = 'Not saved';
-          }
+    checkbox.checked = !!urls.find(url => url.indexOf(name) !== -1);
 
-          cache.keys().then(function(keys) {
-            console.log('keys', keys);
+    checkbox.onclick = function() {
+      if (checkbox.checked) {
+        caches.open('test-SW').then(function(cache) {
+          cache.add(name).then(function() {
+            messageEl.innerHTML = 'Saved';
+
+            cache.keys().then(function(keys) {
+              console.log('keys', keys);
+            });
+          });;
+        })
+      } else {
+        caches.open('test-SW').then(function(cache) {
+          cache.delete(name).then(function(isDeleted) {
+            if (isDeleted) {
+              messageEl.innerHTML = 'Not saved';
+            }
+
+            cache.keys().then(function(keys) {
+              console.log('keys', keys);
+            });
           });
-        });
-      })
+        })
+      }
     }
   }
-}
+});
