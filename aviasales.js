14892b0942c63176f8d1908d21454e44a25480a8
Sergey Jar <sereja@d42.tech>
2024-09-03 14:17:30 +0300

    aviasales apdate

--- aviasales.js
+++ aviasales.js
@@ -1,31 +1,29 @@
-const toket = ''
 
 const data = {
-  token:,
-  origin: 'RMO',
-  destination: 'MIL',
+  token,
+  origin: 'MOW',
+  destination: 'RMO',
   departure_at: '2024-11',
   // return_at: '2024-09',
   sorting: 'price',
   currency: 'EUR',
-  limit: '300',
+  limit: 1000,
   // page: '1',
-  // one_way: true
+  one_way: true
 }
 
 const params = new URLSearchParams(data).toString();
 
 console.log(`https://api.travelpayouts.com/aviasales/v3/prices_for_dates?${params}`);
 
-
 const prices = {
-  token:,
-  origin: 'RMO',
-  destination: 'MIL',
+  token,
+  origin: 'MOW',
+  destination: 'RMO',
   sorting: 'price',
   currency: 'EUR',
   period_type: 'year',
-  limit: '300',
+  limit: 1000,
 }
 
 const paramsForPrices = new URLSearchParams(prices).toString();
